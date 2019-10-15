3d7c13cae932e23be61c91351c5b9cb37419364c
YKB2017 <822041817@qq.com>
2019-10-15 17:58:19 +0800

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,8 +17,9 @@ class UsersTableSeeder extends Seeder
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
         $user = User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        $user->name = '境界的彼方';
+        $user->email = '822041817@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
