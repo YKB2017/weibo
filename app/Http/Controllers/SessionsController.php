1ac03c99558951ddae9c533974ca6d734400e928
YKB2017 <822041817@qq.com>
2019-10-15 10:06:19 +0800

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+
+class SessionsController extends Controller
+{
+    //显示登录页面
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    //登录
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // 登录成功后的相关操作
+            session()->flash('success','欢迎回来！');
+            return redirect()->route('users.show',[Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+    }
+
+    //退出登录
+    public function destroy()
+    {
+
+    }
+}
