85c0d01c563eaaf25e96888a282923b5751f9e2b
YKB2017 <822041817@qq.com>
2019-10-15 15:19:36 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -34,7 +34,30 @@ class UsersController extends Controller
         ]);
 
         Auth::login($user);
-        session()->flash('success','欢迎，你将在这开始一段新的路程~');
-        return redirect()->route('users.show',[$user]);
+        session()->flash('success', '欢迎，你将在这开始一段新的路程~');
+        return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data['name'] = $request->name;
+
+        if ($request->password) {
+            $data['password'] = $request->password;
+        }
+        $user->update($data);
+
+        session()->flash('success','个人资料更新成功！');
+        return redirect()->route('users.show', $user);
     }
 }
